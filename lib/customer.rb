59589cc0e233f4822a8ba48cb1a720619c0ca156
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-08-02 01:57:53 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -20,9 +20,13 @@ class Customer
   def meals
   Meal.all.select do |meal|
     meal.customer == self
+    end
   end
-end
 
-
+  def waiters
+  meals.map do |meal|
+    meal.waiter
+    end
+  end
 
 end
